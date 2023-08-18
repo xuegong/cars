653323034dc9d7a7462a8b1bee9c1a439670779a
Jeff Wang <xgwang88@gmail.com>
2023-08-17 21:41:05 -0600

    unit test

---
+++ src/test/java/com/jeff/cars/CarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
